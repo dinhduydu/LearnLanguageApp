c5f6d081cc8652673ad3d82430d9be4c351d8057
dinhduydu1110 <94332454+dinhduydu@users.noreply.github.com>
2025-07-07 20:05:11 +0700

    [TUTORIAL] Update folder structure

---
+++ learn_language_app/lib/features/vocabulary/presentation/vocabulary_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../../vocabulary/data/vocabulary_list.dart';
+
+class VocabularyPage extends StatelessWidget {
+  const VocabularyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Vocabulary'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: vocabularyList.length,
+        itemBuilder: (context, index) {
+          final item = vocabularyList[index];
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text('${index + 1}'),
+            ),
+            title: Text(item['word'] ?? ''),
+            subtitle: Text(item['meaning'] ?? ''),
+          );
+        },
+      ),
+    );
+  }
+}
