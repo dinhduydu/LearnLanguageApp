2f4a1b61d31fcd2e55e47d80a65cd1a4c5b08d3a
dinhduydu1110 <94332454+dinhduydu@users.noreply.github.com>
2025-07-07 20:09:01 +0700

    [TUTORIAL] Remove redundant folder

--- lib/feature/vocabulary/data/vocabulary_list.dart
+++
@@ -1,7 +0,0 @@
-final List<Map<String, String>> vocabularyList = [
-  {'word': 'こんにちは', 'meaning': 'Hello'},
-  {'word': 'ありがとう', 'meaning': 'Thank you'},
-  {'word': 'さようなら', 'meaning': 'Goodbye'},
-  {'word': '勉強', 'meaning': 'Study'},
-  {'word': '食べる', 'meaning': 'To eat'},
-];
