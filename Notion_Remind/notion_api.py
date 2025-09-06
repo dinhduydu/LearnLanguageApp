63bfbf765c85a2ab1e22444f18c42db2b43dfe66
dinhduydu1110 <94332454+dinhduydu@users.noreply.github.com>
2025-09-06 22:36:49 +0700

    [NOTION] Update

--- Notion_Remind/notion_api.py
+++ Notion_Remind/notion_api.py
@@ -7,10 +7,26 @@ def query_database(database_id: str) -> list:
     res.raise_for_status()
     return res.json().get("results", [])
 
-def get_page_property(props: dict, key: str, ptype: str) -> str:
-    if key not in props:
+def get_page_property(props: dict, name: str, prop_type: str) -> str:
+    """
+    Lấy giá trị property từ Notion page theo type.
+    """
+    if name not in props:
         return ""
-    value = props[key].get(ptype, [])
-    if value and "text" in value[0]:
-        return value[0]["text"]["content"]
-    return ""
+
+    prop = props[name]
+    if prop["type"] != prop_type:
+        return ""
+
+    if prop_type == "title":
+        return "".join([t["plain_text"] for t in prop["title"]])
+    elif prop_type == "rich_text":
+        return "".join([t["plain_text"] for t in prop["rich_text"]])
+    elif prop_type == "multi_select":
+        return ", ".join([t["name"] for t in prop["multi_select"]])
+    elif prop_type == "checkbox":
+        return str(prop["checkbox"])
+    elif prop_type == "date":
+        return prop["date"]["start"] if prop["date"] else ""
+    else:
+        return ""
